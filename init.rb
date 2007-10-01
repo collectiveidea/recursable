5f59b4a2722a95d815e0603eda71e05ffbe20c6a
Daniel Morrison <daniel@collectiveidea.com>
2007-10-01 15:27:50 +0000

    Learning how to spell my own companie's name
    git-svn-id: http://source.collectiveidea.com/public/rails/plugins/recursable@232 2c0bb46f-6917-0410-8ab9-f4bf1162979e

--- init.rb
+++ init.rb
@@ -1,22 +1,26 @@
+require File.join(File.dirname(__FILE__), 'lib', 'enumerable')
+require File.join(File.dirname(__FILE__), 'lib', 'recursable')
+require File.join(File.dirname(__FILE__), 'lib', 'tree')
+
 if defined?(ActiveRecord::Acts::Tree)
   module ActiveRecord::Acts::Tree
-    include ColleciveIdea::Recursable
-    include ColleciveIdea::Recursable::Tree
+    include CollectiveIdea::Recursable
+    include CollectiveIdea::Recursable::Tree
   end
 end
 
 if defined?(ActiveRecord::Acts::NestedSet)
   module ActiveRecord::Acts::NestedSet
-    include ColleciveIdea::Recursable
+    include CollectiveIdea::Recursable
   end
 end
 
 if defined?(SymetrieCom::Acts::NestedSet)
   module SymetrieCom::Acts::NestedSet
-    include ColleciveIdea::Recursable
+    include CollectiveIdea::Recursable
   end
 end
 
 module Enumerable
-  include ColleciveIdea::Enumerable
+  include CollectiveIdea::Enumerable
 end
