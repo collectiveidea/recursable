87b25b601d9efc673b1fb14ac5fbfe58315df0a4
Daniel Morrison <daniel@collectiveidea.com>
2007-10-01 16:06:19 +0000

    Updating Enumerable::recurse to handle not recursable collections
    git-svn-id: http://source.collectiveidea.com/public/rails/plugins/recursable@234 2c0bb46f-6917-0410-8ab9-f4bf1162979e

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,5 +1,11 @@
 module Enumerable
   def recurse(&block)
-    self.each {|x| x.recurse(&block) }
+    self.each do |x| 
+      if x.respond_to?(:recurse)
+        x.recurse(&block)
+      else
+        block.call
+      end
+    end
   end
 end
