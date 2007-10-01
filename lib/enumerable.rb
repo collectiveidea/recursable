0e66a25081898a06d5352c2d4032816ce834f28a
Daniel Morrison <daniel@collectiveidea.com>
2007-10-01 17:24:26 +0000

    Fixing recurse for non-recursable items, and adding a tiny bit of documentation
    git-svn-id: http://source.collectiveidea.com/public/rails/plugins/recursable@235 2c0bb46f-6917-0410-8ab9-f4bf1162979e

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,10 +1,13 @@
+# recurse can be called on any Enumerable set.  If the set has a mix of recursable 
+# and non-recursable items, it will still work, as it will work the same as each()
+# in those cases.
 module Enumerable
   def recurse(&block)
     self.each do |x| 
       if x.respond_to?(:recurse)
         x.recurse(&block)
       else
-        block.call
+        block.call(x)
       end
     end
   end
